eabd71b389606954cb73107daff90db28c8de9d5
boostedgagi <dragan.02jelic@gmail.com>
2023-06-20 23:43:07 +0200

    Minor.

--- tests/Unit/Controller/ExaminationControllerTest.php
+++ tests/Unit/Controller/ExaminationControllerTest.php
@@ -2,12 +2,25 @@
 
 namespace App\Tests\Unit\Controller;
 
+use Nebkam\FluentTest\RequestBuilder;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExaminationControllerTest extends TestCase
 {
-    public function testSomething():void
-    {
-    }
+//    public static ?KernelBrowser $client=null;
+//
+//    public function testGetOneExamination():void
+//    {
+//        $response = RequestBuilder::create(self::$client);
+//
+//        $response
+//            ->setMethod(Request::METHOD_GET)
+//            ->setUri('/examinations/%s',45)
+//            ->getResponse();
+//
+//        dump($response->getResponse()->getJsonContent());
+//    }
 }
