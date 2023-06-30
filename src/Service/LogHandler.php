3a01d03d91d2b6a08ee8c079ac26a42fab45f3b6
boostedgagi <dragan.02jelic@gmail.com>
2023-06-30 14:24:46 +0200

    Location refactor.

--- src/Service/LogHandler.php
+++ src/Service/LogHandler.php
@@ -10,15 +10,13 @@ class LogHandler
     public function getMyLoginLocation(MobileDetectorInterface $detector):Log
     {
         $deviceDetect = new MobileDetectRepository($detector);
-//
-        $ip = getenv("HTTP_X_FORWARDED_FOR");
-        $export = (object)(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip)));
-        $newLog = (object)[
-            'device'=>$deviceDetect->getDeviceInfo(),
-            'country'=>$export->geoplugin_countryName,
-            'ip'=>$export->geoplugin_request
-        ];
 
-        return new Log($newLog->device,$newLog->country,$newLog->ip);
+        $export = (object)(unserialize(file_get_contents('http://www.geoplugin.net/php.gp')));
+
+        return new Log(
+            $deviceDetect->getDeviceInfo(),
+            $export->geoplugin_countryName,
+            $export->geoplugin_request
+        );
     }
 }
