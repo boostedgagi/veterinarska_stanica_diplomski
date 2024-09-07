6f8cb2d3f60946c089b4f0d44263392545fe295b
boostedgagi <dragan.02jelic@gmail.com>
2024-09-07 08:36:18 +0200

    test package update

--- tests/Unit/Controller/HealthRecordControllerTest.php
+++ tests/Unit/Controller/HealthRecordControllerTest.php
@@ -10,20 +10,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HealthRecordControllerTest extends BaseTestCase
 {
-//    public function testMakeHealthRecord(): void
-//    {
-//        $healthRecord = (new HealthRecord())
-//            ->setMadeByVet(false)
-//            ->setStatus('waiting')
-//            ->setNotifiedDayBefore(false)
-//            ->setNotifiedWeekBefore(false)
-//            ->setStartedAt(new DateTime('2024-03-25 20:07:56'))
-//            ->setFinishedAt(new DateTime('2024-03-25 22:07:56'))
-//            ->setAtPresent(false)
-//            ->setExamination(null)
-//            ->setVet(null)
-//            ->setComment('good');
-//
-//        $this->em->persist();
-//    }
+    public function testMakeHealthRecord(): void
+    {
+        $healthRecord = (new HealthRecord())
+            ->setMadeByVet(false)
+            ->setStatus('waiting')
+            ->setNotifiedDayBefore(false)
+            ->setNotifiedWeekBefore(false)
+            ->setStartedAt(new DateTime('2024-03-25 20:07:56'))
+            ->setFinishedAt(new DateTime('2024-03-25 22:07:56'))
+            ->setAtPresent(false)
+            ->setExamination(null)
+            ->setVet(null)
+            ->setComment('good');
+
+        $this->em->persist($healthRecord);
+        $this->em->flush();
+    }
 }
